673648f1853d012f0298b71591a876ca461b7f0c
elxrojo <elxrojo115@gmail.com>
2024-10-17 18:18:08 -0300

    Adding account logic

--- account-service/src/main/java/elxrojo/account_service/controller/AccountController.java
+++ account-service/src/main/java/elxrojo/account_service/controller/AccountController.java
@@ -1,6 +1,9 @@
 package elxrojo.account_service.controller;
 
 
+import elxrojo.account_service.service.IAccountService;
+import elxrojo.account_service.service.implementation.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    IAccountService accountService;
 
     @PostMapping("/")
     public ResponseEntity<String> createAccount(){
+
         return ResponseEntity.ok("Account created successfully");
     }
 }
