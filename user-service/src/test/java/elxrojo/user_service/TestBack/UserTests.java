978fcb1d49dfdde165f3cdf91edf159247e734b5
elxrojo <elxrojo115@gmail.com>
2024-11-10 16:35:13 -0300

    finishing createUser test user-service

--- user-service/src/test/java/elxrojo/user_service/TestBack/UserTests.java
+++ user-service/src/test/java/elxrojo/user_service/TestBack/UserTests.java
@@ -3,6 +3,8 @@ package elxrojo.user_service.TestBack;
 import com.google.gson.JsonObject;
 import org.junit.jupiter.api.*;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -12,23 +14,93 @@ public class UserTests {
     private String baseUrl = "http://localhost:8081/users/";
     static public Integer dni;
     static public Integer phone;
+    static public String email;
+    static public String token;
+
 
     @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @Order(1)
-    @TestClassOrder(ClassOrderer.OrderAnnotation.class)
-    class createUser{
+    class createUser {
 
         @Test
         @Order(1)
-        public void positive(){
+        public void positive() {
+            String uniqueId = UUID.randomUUID().toString().substring(0, 8);
 
             JsonObject userCreated = new JsonObject();
             userCreated.addProperty("firstName", "Test");
-            userCreated.addProperty("lastName", "Test");
+            userCreated.addProperty("lastName", uniqueId);
             dni = generateDni();
             userCreated.addProperty("dni", dni);
-            userCreated.addProperty("email", "test1@gmail.com");
+            email = "test" + uniqueId + "@gmail.com";
+            userCreated.addProperty("email", email);
             phone = generatePhone();
+            userCreated.addProperty("phone", phone);
+            userCreated.addProperty("password", "123456789Lol");
+
+            token = given().contentType("application/json")
+                    .body(userCreated.toString())
+                    .post(baseUrl)
+                    .then()
+                    .statusCode(201)
+                    .body("accessToken", notNullValue())
+                    .extract().path("accessToken");
+        }
+
+        @Test
+        @Order(2)
+        public void existingEmail() {
+            JsonObject userCreated = new JsonObject();
+            userCreated.addProperty("firstName", "Test");
+            userCreated.addProperty("lastName", "FailedTest");
+            Integer generatedDni = generateDni();
+            userCreated.addProperty("dni", generatedDni);
+            userCreated.addProperty("email", email);
+            Integer generatedPhone = generatePhone();
+            userCreated.addProperty("phone", generatedPhone);
+            userCreated.addProperty("password", "123456789Lol");
+
+            given().contentType("application/json")
+                    .body(userCreated.toString())
+                    .post(baseUrl)
+                    .then()
+                    .statusCode(400)
+                    .body("details", equalTo("This email already exists!"));
+        }
+
+        @Test
+        @Order(3)
+        public void existingDNI() {
+
+            JsonObject userCreated = new JsonObject();
+            userCreated.addProperty("firstName", "Test");
+            userCreated.addProperty("lastName", "FailedTest");
+            userCreated.addProperty("dni", dni);
+            userCreated.addProperty("email", "failedTest@gmail.com");
+            Integer generatedPhone = generatePhone();
+            userCreated.addProperty("phone", generatedPhone);
+            userCreated.addProperty("password", "123456789Lol");
+
+            given().contentType("application/json")
+                    .body(userCreated.toString())
+                    .post(baseUrl)
+                    .then()
+                    .statusCode(400)
+                    .body("details", equalTo("This DNI already exists!"));
+        }
+
+        @Test
+        @Order(4)
+        public void existingPhone() {
+            String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+
+            JsonObject userCreated = new JsonObject();
+            userCreated.addProperty("firstName", "Test");
+            userCreated.addProperty("lastName", "FailedTest");
+            Integer generatedDni = generateDni();
+            userCreated.addProperty("dni", generatedDni);
+            userCreated.addProperty("email", "failedTest@gmail.com");
             userCreated.addProperty("phone", phone);
             userCreated.addProperty("password", "123456789Lol");
 
@@ -36,24 +108,20 @@ public class UserTests {
                     .body(userCreated.toString())
                     .post(baseUrl)
                     .then()
-                    .log().body()
-                    .statusCode(201)
-                    .body("token", notNullValue());
+                    .statusCode(400)
+                    .body("details", equalTo("This phone already exists!"));
         }
-
-
     }
 
 
-
-//    Handler functions
+    //    Handler functions
 
     public Integer generateDni() {
         StringBuilder dniGenerated = new StringBuilder();
         for (int i = 0; i < 8; i++) {
             dniGenerated.append((int) (Math.random() * 10));
         }
-        return Integer.parseInt(String.valueOf(dniGenerated)) ;
+        return Integer.parseInt(String.valueOf(dniGenerated));
     }
 
     public Integer generatePhone() {
@@ -61,7 +129,7 @@ public class UserTests {
         for (int i = 0; i < 8; i++) {
             phoneGenerated.append((int) (Math.random() * 10));
         }
-        return Integer.parseInt(String.valueOf(phoneGenerated)) ;
+        return Integer.parseInt(String.valueOf(phoneGenerated));
     }
 
 
