224631eb9c9f02200221a4068038695fcb7764a5
elxrojo <elxrojo115@gmail.com>
2024-11-19 04:47:41 -0300

    Changing some services names for docker

--- transaction-service/src/test/java/elxrojo/transaction_service/TestBack/TransactionTests.java
+++ transaction-service/src/test/java/elxrojo/transaction_service/TestBack/TransactionTests.java
@@ -1,111 +1,111 @@
-package elxrojo.transaction_service.TestBack;
-
-import com.google.gson.JsonObject;
-import org.junit.jupiter.api.*;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
-@TestClassOrder(ClassOrderer.OrderAnnotation.class)
-public class TransactionTests {
-    static private String baseUrl = "http://localhost:8086/transactions/";
-    static public Integer accountId = 1;
-    static public Integer transactionId;
-
-
-    @Order(1)
-    @Nested
-    class createTransaction {
-
-        @Test
-        public void createTransference() {
-            given().post(baseUrl + "create?amount=10&transactionType=2&activityType=1&origin=5130537175978645296140&name=John+Doe&destination=6560721963391433856341&accountId=" + accountId)
-                    .then()
-                    .log().body()
-                    .statusCode(201);
-        }
-
-        @Test
-        public void createDeposit() {
-            given().post(baseUrl + "create?amount=100&transactionType=2&accountId=" + accountId)
-                    .then()
-                    .log().body()
-                    .statusCode(201);
-        }
-
-
-    }
-
-    @Nested
-    @Order(2)
-    class getTransactionsByAccount {
-
-        @Test
-        public void positive() {
-            transactionId = given().get(baseUrl + accountId)
-                    .then()
-                    .statusCode(200)
-                    .body("size()", greaterThanOrEqualTo(1))
-                    .extract().path("id[0]");
-        }
-
-        @Test
-        public void invalidAccountId() {
-            given().get(baseUrl)
-                    .then()
-                    .statusCode(500);
-        }
-
-        @Test
-        public void positiveWithLimit() {
-            given().get(baseUrl + accountId + "?limit=1")
-                    .then()
-                    .statusCode(200)
-                    .body("size()", equalTo(1));
-        }
-    }
-
-    @Nested
-    @Order(3)
-    class getTransactionByAccount {
-
-        @Test
-        public void positive() {
-            given()
-                    .get(baseUrl + transactionId + "/account/" + accountId)
-                    .then()
-                    .statusCode(200)
-                    .log().body()
-                    .body("id", equalTo(transactionId));
-        }
-
-        @Test
-        public void invalidAccountId() {
-            given().get(baseUrl + 100000000 + "/account/" + accountId)
-                    .then()
-                    .statusCode(404);
-        }
-
-        @Test
-        public void unauthorized() {
-            given().get(baseUrl + transactionId + "/account/" + 5)
-                    .then()
-                    .statusCode(401);
-        }
-    }
-
-    @Nested
-    @Order(4)
-    class getLatestDestinations {
-        @Test
-        public void positive() {
-            given()
-                    .get(baseUrl + accountId + "/destination")
-                    .then()
-                    .statusCode(200)
-                    .log().body()
-                    .body("size()", greaterThanOrEqualTo(1));
-        }
-    }
-
-}
+//package elxrojo.transaction_service.TestBack;
+//
+//import com.google.gson.JsonObject;
+//import org.junit.jupiter.api.*;
+//
+//import static io.restassured.RestAssured.given;
+//import static org.hamcrest.Matchers.*;
+//
+//@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+//public class TransactionTests {
+//    static private String baseUrl = "http://localhost:8086/transactions/";
+//    static public Integer accountId = 1;
+//    static public Integer transactionId;
+//
+//
+//    @Order(1)
+//    @Nested
+//    class createTransaction {
+//
+//        @Test
+//        public void createTransference() {
+//            given().post(baseUrl + "create?amount=10&transactionType=2&activityType=1&origin=5130537175978645296140&name=John+Doe&destination=6560721963391433856341&accountId=" + accountId)
+//                    .then()
+//                    .log().body()
+//                    .statusCode(201);
+//        }
+//
+//        @Test
+//        public void createDeposit() {
+//            given().post(baseUrl + "create?amount=100&transactionType=2&accountId=" + accountId)
+//                    .then()
+//                    .log().body()
+//                    .statusCode(201);
+//        }
+//
+//
+//    }
+//
+//    @Nested
+//    @Order(2)
+//    class getTransactionsByAccount {
+//
+//        @Test
+//        public void positive() {
+//            transactionId = given().get(baseUrl + accountId)
+//                    .then()
+//                    .statusCode(200)
+//                    .body("size()", greaterThanOrEqualTo(1))
+//                    .extract().path("id[0]");
+//        }
+//
+//        @Test
+//        public void invalidAccountId() {
+//            given().get(baseUrl)
+//                    .then()
+//                    .statusCode(500);
+//        }
+//
+//        @Test
+//        public void positiveWithLimit() {
+//            given().get(baseUrl + accountId + "?limit=1")
+//                    .then()
+//                    .statusCode(200)
+//                    .body("size()", equalTo(1));
+//        }
+//    }
+//
+//    @Nested
+//    @Order(3)
+//    class getTransactionByAccount {
+//
+//        @Test
+//        public void positive() {
+//            given()
+//                    .get(baseUrl + transactionId + "/account/" + accountId)
+//                    .then()
+//                    .statusCode(200)
+//                    .log().body()
+//                    .body("id", equalTo(transactionId));
+//        }
+//
+//        @Test
+//        public void invalidAccountId() {
+//            given().get(baseUrl + 100000000 + "/account/" + accountId)
+//                    .then()
+//                    .statusCode(404);
+//        }
+//
+//        @Test
+//        public void unauthorized() {
+//            given().get(baseUrl + transactionId + "/account/" + 5)
+//                    .then()
+//                    .statusCode(401);
+//        }
+//    }
+//
+//    @Nested
+//    @Order(4)
+//    class getLatestDestinations {
+//        @Test
+//        public void positive() {
+//            given()
+//                    .get(baseUrl + accountId + "/destination")
+//                    .then()
+//                    .statusCode(200)
+//                    .log().body()
+//                    .body("size()", greaterThanOrEqualTo(1));
+//        }
+//    }
+//
+//}
--- user-service/src/test/java/elxrojo/user_service/TestBack/UserTests.java
+++ user-service/src/test/java/elxrojo/user_service/TestBack/UserTests.java
@@ -1,282 +1,282 @@
-package elxrojo.user_service.TestBack;
-
-import com.google.gson.JsonObject;
-import org.junit.jupiter.api.*;
-
-import java.util.UUID;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
-@TestClassOrder(ClassOrderer.OrderAnnotation.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class UserTests {
-
-    private String baseUrl = "http://localhost:8081/users/";
-    static public Integer dni;
-    static public String id;
-    static public Integer phone;
-    static public String email;
-    static public String token;
-
-
-    @Nested
-    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-    @Order(1)
-    class createUser {
-
-        @Test
-        @Order(1)
-        public void positive() {
-            String uniqueId = UUID.randomUUID().toString().substring(0, 8);
-
-            JsonObject userCreated = new JsonObject();
-            userCreated.addProperty("firstName", "Test");
-            userCreated.addProperty("lastName", uniqueId);
-            dni = generateDni();
-            userCreated.addProperty("dni", dni);
-            email = "test" + uniqueId + "@gmail.com";
-            userCreated.addProperty("email", email);
-            phone = generatePhone();
-            userCreated.addProperty("phone", phone);
-            userCreated.addProperty("password", "123456789Lol");
-
-            var response = given().contentType("application/json")
-                    .body(userCreated.toString())
-                    .post(baseUrl)
-                    .then()
-                    .log().body()
-                    .statusCode(201)
-                    .body("accessToken", notNullValue())
-                    .extract();
-
-            token = response.path("accessToken");
-            id = response.path("user.id");
-        }
-
-        @Test
-        @Order(2)
-        public void existingEmail() {
-            JsonObject userCreated = new JsonObject();
-            userCreated.addProperty("firstName", "Test");
-            userCreated.addProperty("lastName", "FailedTest");
-            Integer generatedDni = generateDni();
-            userCreated.addProperty("dni", generatedDni);
-            userCreated.addProperty("email", email);
-            Integer generatedPhone = generatePhone();
-            userCreated.addProperty("phone", generatedPhone);
-            userCreated.addProperty("password", "123456789Lol");
-
-            given().contentType("application/json")
-                    .body(userCreated.toString())
-                    .post(baseUrl)
-                    .then()
-                    .statusCode(400)
-                    .body("details", equalTo("This email already exists!"));
-        }
-
-        @Test
-        @Order(3)
-        public void existingDNI() {
-
-            JsonObject userCreated = new JsonObject();
-            userCreated.addProperty("firstName", "Test");
-            userCreated.addProperty("lastName", "FailedTest");
-            userCreated.addProperty("dni", dni);
-            userCreated.addProperty("email", "failedTest@gmail.com");
-            Integer generatedPhone = generatePhone();
-            userCreated.addProperty("phone", generatedPhone);
-            userCreated.addProperty("password", "123456789Lol");
-
-            given().contentType("application/json")
-                    .body(userCreated.toString())
-                    .post(baseUrl)
-                    .then()
-                    .statusCode(400)
-                    .body("details", equalTo("This DNI already exists!"));
-        }
-
-        @Test
-        @Order(4)
-        public void existingPhone() {
-            String uniqueId = UUID.randomUUID().toString().substring(0, 8);
-
-            JsonObject userCreated = new JsonObject();
-            userCreated.addProperty("firstName", "Test");
-            userCreated.addProperty("lastName", "FailedTest");
-            Integer generatedDni = generateDni();
-            userCreated.addProperty("dni", generatedDni);
-            userCreated.addProperty("email", "failedTest@gmail.com");
-            userCreated.addProperty("phone", phone);
-            userCreated.addProperty("password", "123456789Lol");
-
-            given().contentType("application/json")
-                    .body(userCreated.toString())
-                    .post(baseUrl)
-                    .then()
-                    .statusCode(400)
-                    .body("details", equalTo("This phone already exists!"));
-        }
-    }
-
-    @Nested
-    @Order(3)
-    class login {
-
-        @Test
-        public void positive() {
-            JsonObject dataLogin = new JsonObject();
-            dataLogin.addProperty("email", email);
-            dataLogin.addProperty("password", "123456789Lol");
-
-            token = given().contentType("application/json")
-                    .body(dataLogin.toString())
-                    .post(baseUrl + "login")
-                    .then()
-                    .log().body()
-                    .statusCode(200)
-                    .body("token", notNullValue())
-                    .extract().path("token");
-        }
-
-        @Test
-        public void invalidEmail() {
-            JsonObject dataLogin = new JsonObject();
-            dataLogin.addProperty("email", "asddaasddassdaasdadsdas@correo.com");
-            dataLogin.addProperty("password", "123456789Lol");
-
-            given().contentType("application/json")
-                    .body(dataLogin.toString())
-                    .post(baseUrl + "login")
-                    .then()
-                    .log().body()
-                    .statusCode(404)
-                    .body("details", equalTo("Non-existent user :/"));
-        }
-
-        @Test
-        public void invalidPassword() {
-            JsonObject dataLogin = new JsonObject();
-            dataLogin.addProperty("email", email);
-            dataLogin.addProperty("password", "jkashdaskjhdaskjhdas782782");
-
-            given().contentType("application/json")
-                    .body(dataLogin.toString())
-                    .post(baseUrl + "login")
-                    .then()
-                    .log().body()
-                    .statusCode(401)
-                    .body("details", equalTo("Incorrect password :/"));
-        }
-
-        @Test
-        public void incompleteLogin() {
-            JsonObject dataLogin = new JsonObject();
-            dataLogin.addProperty("email", "");
-            dataLogin.addProperty("password", "");
-
-            given().contentType("application/json")
-                    .body(dataLogin.toString())
-                    .post(baseUrl + "login")
-                    .then()
-                    .log().body()
-                    .statusCode(400)
-                    .body("details", equalTo("Incomplete login information"));
-        }
-    }
-
-
-    @Order(4)
-    @Nested
-    class logout {
-        @Test
-        public void positive() {
-            given().header("Authorization", "Bearer " + token)
-                    .post(baseUrl + "logout")
-                    .then()
-                    .log().body()
-                    .statusCode(200);
-        }
-    }
-
-
-    @Nested
-    @Order(5)
-    class getUserById {
-
-        @Test
-        public void positive() {
-            System.out.println(id);
-            given()
-                    .get(baseUrl + id)
-                    .then()
-                    .statusCode(200)
-                    .body("dni", equalTo(dni));
-        }
-
-        @Test
-        public void notFound() {
-            given()
-                    .get(baseUrl + 1)
-                    .then()
-                    .statusCode(404);
-        }
-    }
-
-
-    @Nested
-    @Order(6)
-    class updateUser {
-
-        @Test
-        public void positive() {
-            JsonObject userUpdate = new JsonObject();
-            userUpdate.addProperty("firstName", "TestModified");
-
-            given()
-                    .contentType("application/json")
-                    .body(userUpdate.toString())
-                    .patch(baseUrl + id)
-                    .then()
-                    .log().body()
-                    .statusCode(200)
-                    .body("firstName", equalTo("TestModified"))
-                    .body("email", equalTo(email));
-        }
-
-        @Test
-        public void cannotEditPassword() {
-            JsonObject userUpdate = new JsonObject();
-            userUpdate.addProperty("password", "asd");
-
-            given()
-                    .contentType("application/json")
-                    .body(userUpdate.toString())
-                    .patch(baseUrl + id)
-                    .then()
-                    .log().body()
-                    .statusCode(400);
-        }
-    }
-
-
-    //    Handler functions
-
-    public Integer generateDni() {
-        StringBuilder dniGenerated = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
-            dniGenerated.append((int) (Math.random() * 10));
-        }
-        return Integer.parseInt(String.valueOf(dniGenerated));
-    }
-
-    public Integer generatePhone() {
-        StringBuilder phoneGenerated = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
-            phoneGenerated.append((int) (Math.random() * 10));
-        }
-        return Integer.parseInt(String.valueOf(phoneGenerated));
-    }
-
-
-}
+//package elxrojo.user_service.TestBack;
+//
+//import com.google.gson.JsonObject;
+//import org.junit.jupiter.api.*;
+//
+//import java.util.UUID;
+//
+//import static io.restassured.RestAssured.given;
+//import static org.hamcrest.Matchers.*;
+//
+//@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//public class UserTests {
+//
+//    private String baseUrl = "http://localhost:8081/users/";
+//    static public Integer dni;
+//    static public String id;
+//    static public Integer phone;
+//    static public String email;
+//    static public String token;
+//
+//
+//    @Nested
+//    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//    @Order(1)
+//    class createUser {
+//
+//        @Test
+//        @Order(1)
+//        public void positive() {
+//            String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+//
+//            JsonObject userCreated = new JsonObject();
+//            userCreated.addProperty("firstName", "Test");
+//            userCreated.addProperty("lastName", uniqueId);
+//            dni = generateDni();
+//            userCreated.addProperty("dni", dni);
+//            email = "test" + uniqueId + "@gmail.com";
+//            userCreated.addProperty("email", email);
+//            phone = generatePhone();
+//            userCreated.addProperty("phone", phone);
+//            userCreated.addProperty("password", "123456789Lol");
+//
+//            var response = given().contentType("application/json")
+//                    .body(userCreated.toString())
+//                    .post(baseUrl)
+//                    .then()
+//                    .log().body()
+//                    .statusCode(201)
+//                    .body("accessToken", notNullValue())
+//                    .extract();
+//
+//            token = response.path("accessToken");
+//            id = response.path("user.id");
+//        }
+//
+//        @Test
+//        @Order(2)
+//        public void existingEmail() {
+//            JsonObject userCreated = new JsonObject();
+//            userCreated.addProperty("firstName", "Test");
+//            userCreated.addProperty("lastName", "FailedTest");
+//            Integer generatedDni = generateDni();
+//            userCreated.addProperty("dni", generatedDni);
+//            userCreated.addProperty("email", email);
+//            Integer generatedPhone = generatePhone();
+//            userCreated.addProperty("phone", generatedPhone);
+//            userCreated.addProperty("password", "123456789Lol");
+//
+//            given().contentType("application/json")
+//                    .body(userCreated.toString())
+//                    .post(baseUrl)
+//                    .then()
+//                    .statusCode(400)
+//                    .body("details", equalTo("This email already exists!"));
+//        }
+//
+//        @Test
+//        @Order(3)
+//        public void existingDNI() {
+//
+//            JsonObject userCreated = new JsonObject();
+//            userCreated.addProperty("firstName", "Test");
+//            userCreated.addProperty("lastName", "FailedTest");
+//            userCreated.addProperty("dni", dni);
+//            userCreated.addProperty("email", "failedTest@gmail.com");
+//            Integer generatedPhone = generatePhone();
+//            userCreated.addProperty("phone", generatedPhone);
+//            userCreated.addProperty("password", "123456789Lol");
+//
+//            given().contentType("application/json")
+//                    .body(userCreated.toString())
+//                    .post(baseUrl)
+//                    .then()
+//                    .statusCode(400)
+//                    .body("details", equalTo("This DNI already exists!"));
+//        }
+//
+//        @Test
+//        @Order(4)
+//        public void existingPhone() {
+//            String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+//
+//            JsonObject userCreated = new JsonObject();
+//            userCreated.addProperty("firstName", "Test");
+//            userCreated.addProperty("lastName", "FailedTest");
+//            Integer generatedDni = generateDni();
+//            userCreated.addProperty("dni", generatedDni);
+//            userCreated.addProperty("email", "failedTest@gmail.com");
+//            userCreated.addProperty("phone", phone);
+//            userCreated.addProperty("password", "123456789Lol");
+//
+//            given().contentType("application/json")
+//                    .body(userCreated.toString())
+//                    .post(baseUrl)
+//                    .then()
+//                    .statusCode(400)
+//                    .body("details", equalTo("This phone already exists!"));
+//        }
+//    }
+//
+//    @Nested
+//    @Order(3)
+//    class login {
+//
+//        @Test
+//        public void positive() {
+//            JsonObject dataLogin = new JsonObject();
+//            dataLogin.addProperty("email", email);
+//            dataLogin.addProperty("password", "123456789Lol");
+//
+//            token = given().contentType("application/json")
+//                    .body(dataLogin.toString())
+//                    .post(baseUrl + "login")
+//                    .then()
+//                    .log().body()
+//                    .statusCode(200)
+//                    .body("token", notNullValue())
+//                    .extract().path("token");
+//        }
+//
+//        @Test
+//        public void invalidEmail() {
+//            JsonObject dataLogin = new JsonObject();
+//            dataLogin.addProperty("email", "asddaasddassdaasdadsdas@correo.com");
+//            dataLogin.addProperty("password", "123456789Lol");
+//
+//            given().contentType("application/json")
+//                    .body(dataLogin.toString())
+//                    .post(baseUrl + "login")
+//                    .then()
+//                    .log().body()
+//                    .statusCode(404)
+//                    .body("details", equalTo("Non-existent user :/"));
+//        }
+//
+//        @Test
+//        public void invalidPassword() {
+//            JsonObject dataLogin = new JsonObject();
+//            dataLogin.addProperty("email", email);
+//            dataLogin.addProperty("password", "jkashdaskjhdaskjhdas782782");
+//
+//            given().contentType("application/json")
+//                    .body(dataLogin.toString())
+//                    .post(baseUrl + "login")
+//                    .then()
+//                    .log().body()
+//                    .statusCode(401)
+//                    .body("details", equalTo("Incorrect password :/"));
+//        }
+//
+//        @Test
+//        public void incompleteLogin() {
+//            JsonObject dataLogin = new JsonObject();
+//            dataLogin.addProperty("email", "");
+//            dataLogin.addProperty("password", "");
+//
+//            given().contentType("application/json")
+//                    .body(dataLogin.toString())
+//                    .post(baseUrl + "login")
+//                    .then()
+//                    .log().body()
+//                    .statusCode(400)
+//                    .body("details", equalTo("Incomplete login information"));
+//        }
+//    }
+//
+//
+//    @Order(4)
+//    @Nested
+//    class logout {
+//        @Test
+//        public void positive() {
+//            given().header("Authorization", "Bearer " + token)
+//                    .post(baseUrl + "logout")
+//                    .then()
+//                    .log().body()
+//                    .statusCode(200);
+//        }
+//    }
+//
+//
+//    @Nested
+//    @Order(5)
+//    class getUserById {
+//
+//        @Test
+//        public void positive() {
+//            System.out.println(id);
+//            given()
+//                    .get(baseUrl + id)
+//                    .then()
+//                    .statusCode(200)
+//                    .body("dni", equalTo(dni));
+//        }
+//
+//        @Test
+//        public void notFound() {
+//            given()
+//                    .get(baseUrl + 1)
+//                    .then()
+//                    .statusCode(404);
+//        }
+//    }
+//
+//
+//    @Nested
+//    @Order(6)
+//    class updateUser {
+//
+//        @Test
+//        public void positive() {
+//            JsonObject userUpdate = new JsonObject();
+//            userUpdate.addProperty("firstName", "TestModified");
+//
+//            given()
+//                    .contentType("application/json")
+//                    .body(userUpdate.toString())
+//                    .patch(baseUrl + id)
+//                    .then()
+//                    .log().body()
+//                    .statusCode(200)
+//                    .body("firstName", equalTo("TestModified"))
+//                    .body("email", equalTo(email));
+//        }
+//
+//        @Test
+//        public void cannotEditPassword() {
+//            JsonObject userUpdate = new JsonObject();
+//            userUpdate.addProperty("password", "asd");
+//
+//            given()
+//                    .contentType("application/json")
+//                    .body(userUpdate.toString())
+//                    .patch(baseUrl + id)
+//                    .then()
+//                    .log().body()
+//                    .statusCode(400);
+//        }
+//    }
+//
+//
+//    //    Handler functions
+//
+//    public Integer generateDni() {
+//        StringBuilder dniGenerated = new StringBuilder();
+//        for (int i = 0; i < 8; i++) {
+//            dniGenerated.append((int) (Math.random() * 10));
+//        }
+//        return Integer.parseInt(String.valueOf(dniGenerated));
+//    }
+//
+//    public Integer generatePhone() {
+//        StringBuilder phoneGenerated = new StringBuilder();
+//        for (int i = 0; i < 8; i++) {
+//            phoneGenerated.append((int) (Math.random() * 10));
+//        }
+//        return Integer.parseInt(String.valueOf(phoneGenerated));
+//    }
+//
+//
+//}
