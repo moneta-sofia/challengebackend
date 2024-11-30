46ed510788f9dee792c86c2c753ce43d179d13eb
elxrojo <elxrojo115@gmail.com>
2024-11-30 17:34:28 -0300

    Adding swagger to account-service

--- account-service/src/main/java/elxrojo/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/elxrojo/account_service/AccountServiceApplication.java
@@ -1,8 +1,17 @@
 package elxrojo.account_service;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.headers.Header;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +20,17 @@ public class AccountServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("basicScheme", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic"))
+						.addParameters("myHeader1", new Parameter().in("header").schema(new StringSchema()).name("myHeader1")).addHeaders("myHeader2", new Header().description("myHeader2 header").schema(new StringSchema())))
+				.info(new Info()
+						.title("Account Service")
+						.version(appVersion)
+						.description("This API is part of the Backend Challenge project, designed to manage user operations and services. It includes endpoints for registration, authentication, and user data management, ensuring secure and efficient handling of requests."));
+	}
+
 
 }
